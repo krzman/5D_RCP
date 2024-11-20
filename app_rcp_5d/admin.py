fde8f30381451000a541c514c412daf0855ea953
Krzysztof Mański <krzysztof.manski@gmail.com>
2024-11-20 12:15:30 +0100

    new model

--- app_rcp_5d/admin.py
+++ app_rcp_5d/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from app_rcp_5d.models import Construcion, RCPDevice
 
 # Register your models here.
+admin.site.register(Construcion)
+admin.site.register(RCPDevice)
--- app_rcp_5d/models.py
+++ app_rcp_5d/models.py
@@ -11,6 +11,9 @@ class Users(models.Model):
 class RCPDevice(models.Model):
     nr = models.IntegerField(unique=True, blank=False, null=False)
 
+    def __str__(self):
+        return str(self.nr)
+
 
 class Construcion(models.Model):
     name = models.CharField(max_length=64, blank=False, null=False)
@@ -18,9 +21,13 @@ class Construcion(models.Model):
     manager = models.OneToOneField(Users, on_delete=models.CASCADE)
     rfid_device = models.ForeignKey(RCPDevice, on_delete=models.CASCADE)
 
+    def __str__(self):
+        return self.name
 
-# class TOYOTA(models.Model):
-#     construcion = models.OneToOneField(Construcion, on_delete=models.CASCADE)
-#     user = models.ManyToManyField(User, on_delete=models.CASCADE)
-#     date = models.DateField(blank=False, null=False, auto_now_add=True)
-#     time = models.TimeField(blank=False, null=False, auto_now_add=True)
+
+class RCPList(models.Model):
+    construcion = models.OneToOneField(Construcion, on_delete=models.CASCADE)
+    rfid_device = models.OneToOneField(RCPDevice, on_delete=models.CASCADE)
+    user = models.ManyToManyField(User)
+    date = models.DateField(blank=False, null=False, auto_now_add=True)
+    time = models.TimeField(blank=False, null=False, auto_now_add=True)
